443dcdb73189468631fa89c6e0a0dd03b4d5d869
fookinbarrie <your-email@example.com>
2026-03-13 05:54:13 +0700

    chore: update vercel analytics and cleanup unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AudioControlProvider } from './AudioControlContext';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
