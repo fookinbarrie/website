1944dde7fdcbf3f215b423ac8d138d7bd728516c
fookinbarrie <your-email@example.com>
2026-03-12 19:55:25 +0700

    perf: implement manual chunk splitting to optimize build bundle

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('reactflow')) return 'vendor-reactflow';
+            if (id.includes('@blocknote')) return 'vendor-blocknote';
+            if (id.includes('react') || id.includes('react-dom')) return 'vendor-react';
+            return 'vendor-others';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
